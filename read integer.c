1cfe99051d13f2802bad5e8edf1f0520257b6f27
Abhishek Gautam <94949116+abhipandit3@users.noreply.github.com>
2022-12-14 22:04:20 +0530

    Add files via upload

---
+++ read integer.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int num;
+    float num2;
+    printf("enter the number");
+    scanf("%d",&num);
+    printf("enter the number");
+    scanf("%f",&num2);
+    printf("%d",num);
+    printf("%f",num2);
+}
