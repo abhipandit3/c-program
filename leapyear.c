32ad165ef0f023947b178c2683d75aebefc81fd1
Abhishek Gautam <94949116+abhipandit3@users.noreply.github.com>
2022-12-20 00:40:02 +0530

    Add files via upload

---
+++ leapyear.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int year;
+    printf("enter the year");
+    scanf("%d",&year);
+    if(((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0)) {
+        printf("leap year");
+        
+    }
+    else {
+        printf("it is not leap year");
+    }
+}
